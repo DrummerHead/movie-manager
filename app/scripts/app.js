f5b815cd317609d2caa7caa5c6a995cf6f9a675f
DrummerHead <drummerhead@gmail.com>
2014-06-08 15:10:45 -0300

    Jsonp call from omdbapi to get basic movie info

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,38 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name appApp
- * @description
- * # appApp
- *
- * Main module of the application.
- */
-angular
-  .module('appApp', []);
+var mov = angular.module('mov', []);
+
+/* Controllers
+ * ========================================= */
+
+mov.controller('MainCtrl', function ($scope, $http, GetRawList) {
+  GetRawList.getRaw().then(function(data){
+    $scope.rawMovies = data;
+    $scope.movies = [];
+    $scope.rawMovies.forEach(function(el, i){
+      $http.jsonp('http://www.omdbapi.com/?i=' + el + '&callback=JSON_CALLBACK').then(function(result){
+        $scope.movies.push(result.data);
+      });
+    });
+  });
+});
+
+/* End Controllers */
+
+
+
+
+/* Services
+ * ========================================= */
+
+mov.factory('GetRawList', function($http){
+  return {
+    getRaw : function(){
+      return $http.get('scripts/movies.json').then(function(result){
+        return result.data;
+      });
+    }
+  };
+});
+
+/* End Services */
