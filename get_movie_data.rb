fd2cddcabad0a9d3090fb62ac4b4fbc624d776e7
DrummerHead <drummerhead@gmail.com>
2014-06-10 16:20:39 -0300

    Update script to remove watched movies from array

--- get_movie_data.rb
+++ get_movie_data.rb
@@ -1,9 +1,14 @@
 #!/usr/bin/env ruby
 
 find_omdb = %x[find .. -name "*.omdb"].split("\n")
+find_omdb_watched = %x[find ../_watched -name "*.omdb"].split("\n")
 
-ids = find_omdb.map{ |line|
-  line.gsub(/^\.\.\/[^\/]*\/([^.]*)\.omdb/, '\1')
-}
+def get_id_array(find_result)
+  find_result.map{ |line|
+    line.gsub(/^\.\.\/(_watched\/)?[^\/]*\/([^.]*)\.omdb/, '\2')
+  }
+end
 
-File.write("app/scripts/movies.json", ids.inspect)
+unwatched_movies = get_id_array(find_omdb) - get_id_array(find_omdb_watched)
+
+File.write("app/scripts/movies.json", unwatched_movies)
