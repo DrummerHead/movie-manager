91bcf67baae1b6ec1a903188b1a68cdd5b18279c
DrummerHead <drummerhead@gmail.com>
2014-06-16 19:12:29 -0300

    Contemplate empty omdb files

--- get_movie_data.rb
+++ get_movie_data.rb
@@ -2,16 +2,25 @@
 
 omdb_paths = %x[find .. -name "*.omdb"].split("\n").reject{ |path| path =~ /_watched/}
 
-movies_json = omdb_paths.map.with_index do |path, index|
+omdb_files = []
+
+omdb_paths.each do |path|
   file = File.read(path)
 
+  unless file.empty?
+    omdb_files << file
+  end
+end
+
+movies_json = omdb_files.map.with_index do |file, index|
   if index == 0
     "[" + file + ","
-  elsif index == omdb_paths.length - 1
+  elsif index == omdb_files.length - 1
     file + "]"
   else
     file + ","
   end
 end
 
+puts movies_json
 File.write("app/scripts/movies.json", movies_json.reduce(:+))
